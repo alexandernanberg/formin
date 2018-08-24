7d0a91d150a571ce25806a79775de0f0b07a0d91
Alexander Nanberg <alexander.nanberg@strateg.se>
2018-08-24 19:37:27 +0200

    tests: add more tests

--- src/__tests__/formin.test.js
+++ src/__tests__/formin.test.js
@@ -3,24 +3,41 @@ import React from 'react'
 import { render, fireEvent } from 'react-testing-library'
 import Formin from '..'
 
-function setup({ renderFn, ...props } = {}) {
+jest.useFakeTimers()
+
+function setup({ renderFn, formProps, ...props } = {}) {
   function defaultRenderFn({ getFormProps, getInputProps }) {
     return (
-      <form {...getFormProps()}>
-        <input data-testid="input" {...getInputProps({ name: 'name' })} />
+      <form data-testid="form" {...getFormProps(formProps)}>
+        <input
+          data-testid="input"
+          {...getInputProps({ name: 'name' })}
+          required
+        />
+        <input
+          type="number"
+          data-testid="number"
+          {...getInputProps({ name: 'age' })}
+        />
         <select data-testid="select" {...getInputProps({ name: 'pet' })}>
           <option value="dog">Dog</option>
           <option value="cat">Cat</option>
         </select>
+        <input
+          data-testid="checkbox"
+          type="checkbox"
+          {...getInputProps({ name: 'toys' })}
+        />
         <button type="submit" data-testid="button" />
       </form>
     )
   }
 
   let renderArg
+  const children = renderFn || defaultRenderFn
   const childrenSpy = jest.fn((controllerArg) => {
     renderArg = controllerArg
-    return renderFn || defaultRenderFn(controllerArg)
+    return children(controllerArg)
   })
 
   const utils = render(<Formin {...props}>{childrenSpy}</Formin>)
@@ -29,7 +46,10 @@ function setup({ renderFn, ...props } = {}) {
     ...utils,
     renderArg,
     childrenSpy,
+    form: utils.queryByTestId('form'),
     input: utils.queryByTestId('input'),
+    number: utils.queryByTestId('number'),
+    checkbox: utils.queryByTestId('checkbox'),
     button: utils.queryByTestId('button'),
   }
 }
@@ -78,6 +98,75 @@ test('onFocus sets touched', () => {
   )
 })
 
-// TODO: Check focused element on error (should be first invalid)
+test('getFormProps onSubmit is called', () => {
+  const onSubmit = jest.fn(({ values }) => values)
+  const { renderArg, form } = setup({
+    formProps: {
+      onSubmit,
+    },
+  })
 
-// TODO: Controlled vs uncontrolled
+  fireEvent.submit(form)
+
+  expect(onSubmit.mock.calls[0][0]).toEqual(renderArg)
+})
+
+test('works when controlled', () => {
+  const onChange = jest.fn()
+  const { input } = setup({ onChange, values: { name: 'Alex' } })
+
+  expect(input.value).toEqual('Alex')
+
+  fireEvent.change(input, { target: { value: 'Charlie' } })
+
+  expect(onChange).toHaveBeenCalledWith({ name: 'Charlie' })
+})
+
+test('sets error state when invalid', () => {
+  const onStateChange = jest.fn()
+  const { form } = setup({ onStateChange })
+
+  form.checkValidity()
+  jest.runAllTimers()
+
+  expect(onStateChange).toHaveBeenCalledWith(
+    expect.objectContaining({ errors: { name: 'Constraints not satisfied' } }),
+  )
+})
+
+test('clears error on input change', () => {
+  const onStateChange = jest.fn()
+  const { form, input } = setup({ onStateChange })
+
+  form.checkValidity()
+  jest.runAllTimers()
+
+  fireEvent.change(input, { target: { value: 'Charlie' } })
+
+  expect(onStateChange).toHaveBeenLastCalledWith(
+    expect.objectContaining({
+      errors: {
+        name: false,
+      },
+    }),
+  )
+})
+
+test('calls setInternalState callback', () => {
+  const callback = jest.fn()
+  const { renderArg } = setup()
+
+  renderArg.setState({ values: { name: 'Fido' } }, callback)
+
+  expect(callback).toHaveBeenCalled()
+})
+
+test('throws if name is not provided to getInputProps', () => {
+  expect(() =>
+    setup({
+      renderFn: ({ getInputProps }) => <input {...getInputProps()} />,
+    }),
+  ).toThrow()
+})
+
+// TODO: Check focused element on error (should be first invalid)
